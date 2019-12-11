82962c6dfc3ef7cdc0af6049be05c76b07ae7214
weisong <844159021@qq.com>
2019-12-11 14:57:58 +0800

    修改

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -1,4 +1,56 @@
 package com.zeroten.common.util;
 
-public class CheckUtils {
+public  class CheckUtils {
+    public static boolean isAnyEmpty(String... strings){
+        /*
+        * String... strings 为可变参数，你可以认为它是 String[] strings。如果 strings 中有任
+        意⼀个字符串为空，则返回 false，否则返回 true。
+        * */
+        for (int i=0;i<strings.length;i++)
+        {
+             if (strings[i].indexOf(' ')!=-1)
+             {
+                 return false;
+             }
+        }
+        return true;
+    }
+
+    public static boolean isEmpty(Object[] arr){
+        /*
+        *判断引⽤类型数组是否为空，为空或 null 则返回 true，否则返回 false。
+        *  */
+        if (arr==null||arr.length==0)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+
+    }
+    public static boolean equals(String str1, String str2)
+    {
+        /*判断 str1 字符串是否相等，相等则返回 true，否则返回 false。当其中⼀个是 null 时返回
+        false。*/
+        if(str1==null||str2==null)
+        {
+             return false;
+        }else
+        {
+            return str1.equals(str2);
+        }
+
+    }
+    public static boolean git(Integer n1, Integer n2){
+
+        if(n1==null||n2==null)
+        {
+            return false;
+        }else
+        {
+            return n1.equals(n2);
+        }
+    }
+
 }
--- src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -1,4 +1,42 @@
 package com.zeroten.common.util;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 public class CheckUtilsTest {
+    @Test
+    public  void TestAnyEmpty()
+    {
+        Assert.assertEquals(CheckUtils.isAnyEmpty("sss","张三"),true);
+        Assert.assertEquals(CheckUtils.isAnyEmpty("","张三"),false);
+        Assert.assertEquals(CheckUtils.isAnyEmpty("sss","张三"),false);
+    }
+    @Test
+    public  void TestEmpty()
+    {
+        Object[] obj={};
+        Object[] obj1={123,"456",789};
+        Assert.assertEquals(CheckUtils.isEmpty(obj),true);
+        Assert.assertEquals(CheckUtils.isEmpty(obj1),false);
+    }
+    @Test
+    public  void TestEquals()
+    {
+        Assert.assertEquals(CheckUtils.equals("hei","hei"),true);
+        Assert.assertEquals(CheckUtils.equals("hei","word"),true);
+    }
+    @Test
+    public  void TestEquals1()
+    {
+        /*
+       判断 n1 和 n2 的值是否相等，相等则返回 true，否则返回 false。当其中⼀个是 null 时返回 false。
+
+        */
+        int  i=3;
+        Integer i2=3;
+        Assert.assertEquals(CheckUtils.git(i,i2),true);
+        Assert.assertEquals(CheckUtils.git(i,new Integer(3)),true);
+    }
 }
